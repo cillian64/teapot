e5a451aeae9562f233264dea92b634bb96df7624
David Turner <cillian64@googlemail.com>
2016-03-19 11:30:33 +0000

    Filled out RFM69 transmission and basics

--- firmware/rfm69.c
+++ firmware/rfm69.c
@@ -19,42 +19,49 @@
 /* For some weird reason libopencm3 only provides spi_read8/send8 for f03
  * devices, not for l0. So we recreate them here */
 #define SPI_DR8(spi_base) MMIO8((spi_base)+0x0c)
-void spi_send8(uint32_t spi, uint8_t data);
-uint8_t spi_read8(uint32_t spi);
+static void spi_send8(uint32_t spi, uint8_t data);
+static uint8_t spi_read8(uint32_t spi);
 
 /* Send and receive 8 bits, synchronously */
-uint8_t _rfm69_spi_xfer8(uint8_t data);
+static uint8_t _rfm69_spi_xfer8(uint8_t data);
 
 /* Write a byte to a register */
-void _rfm69_writereg(uint8_t address, uint8_t data);
+static void _rfm69_writereg(uint8_t address, uint8_t data);
 
 /* Read a byte from a register */
-uint8_t _rfm69_readreg(uint8_t address);
+static uint8_t _rfm69_readreg(uint8_t address);
 
 /* Bulk write to registers from a buffer */
-void _rfm69_bulkwrite(uint8_t address, uint8_t *buffer, uint8_t len);
+static void _rfm69_bulkwrite(uint8_t address, uint8_t *buffer, uint8_t len);
 
 /* Bulk read from registers into a buffer */
-void _rfm69_bulkread(uint8_t address, uint8_t *buffer, uint8_t len);
+static void _rfm69_bulkread(uint8_t address, uint8_t *buffer, uint8_t len);
+
+/* Set op mode */
+static void _rfm69_setmode(uint8_t mode);
+
+/* Get op mode */
+static uint8_t _rfm69_getmode(void);
 
 /************* Internal function definitions ***********/
-void spi_send8(uint32_t spi, uint8_t data)
+static void spi_send8(uint32_t spi, uint8_t data)
 {
     SPI_DR8(spi) = data;
 }
-uint8_t spi_read8(uint32_t spi)
+
+static uint8_t spi_read8(uint32_t spi)
 {
     while(!(SPI_SR(spi) & SPI_SR_RXNE));
     return SPI_DR8(spi);
 }
 
-uint8_t _rfm69_spi_xfer8(uint8_t data)
+static uint8_t _rfm69_spi_xfer8(uint8_t data)
 {
     spi_send8(SPI1, data);
     return spi_read8(SPI1);
 }
 
-void _rfm69_writereg(uint8_t address, uint8_t data)
+static void _rfm69_writereg(uint8_t address, uint8_t data)
 {
     gpio_clear(RFM_NSS_PORT, RFM_NSS);
     (void)_rfm69_spi_xfer8(address | (1<<7)); /* Set MSB to write */
@@ -62,7 +69,7 @@ void _rfm69_writereg(uint8_t address, uint8_t data)
     gpio_set(RFM_NSS_PORT, RFM_NSS);
 }
 
-uint8_t _rfm69_readreg(uint8_t address)
+static uint8_t _rfm69_readreg(uint8_t address)
 {
     uint8_t data;
     gpio_clear(RFM_NSS_PORT, RFM_NSS);
@@ -72,7 +79,7 @@ uint8_t _rfm69_readreg(uint8_t address)
     return data;
 }
 
-void _rfm69_bulkwrite(uint8_t address, uint8_t *buffer, uint8_t len)
+static void _rfm69_bulkwrite(uint8_t address, uint8_t *buffer, uint8_t len)
 {
     uint8_t i;
     gpio_clear(RFM_NSS_PORT, RFM_NSS);
@@ -82,7 +89,7 @@ void _rfm69_bulkwrite(uint8_t address, uint8_t *buffer, uint8_t len)
     gpio_set(RFM_NSS_PORT, RFM_NSS);
 }
 
-void _rfm69_bulkread(uint8_t address, uint8_t *buffer, uint8_t len)
+static void _rfm69_bulkread(uint8_t address, uint8_t *buffer, uint8_t len)
 {
     uint8_t i;
     gpio_clear(RFM_NSS_PORT, RFM_NSS);
@@ -114,32 +121,85 @@ void rfm69_init(void)
 
     spi_enable(SPI1);
 
-    /* Wait for RFM to warm up, then check we're in sleep mode */
+    /* Wait for RFM to warm up */
     delay_ms(10);
-    rfm69_opmode(RFM69_OPMODE_SLEEP);
+
+    /* Set the frequency:
+     * Fstep = Fxosc / 2^19 = 32e6 / 2^19
+     * freq = Fstep * FRF
+     * FRF = freq / (32e6 / 2^19)
+     *     = 869.5 / 32 * 2^19 = 14245888 */
+    rfm69_setfreq(14245888);
 }
 
 /* Set frequency.  The value provided is not a frequency in Hz, it is he FRF
  * value put in the register.  See the datasheet for conversion */
 void rfm69_setfreq(uint32_t frf)
 {
-    panic();
+    /* Check we're asleep */
+    _rfm69_setmode(RFM69_OPMODE_SLEEP);
+
+    /* Program 3 FRF registers */
+    _rfm69_writereg(RFM69_REGFRFMSB, (frf << 16) & 0xff);
+    _rfm69_writereg(RFM69_REGFRFMID, (frf << 8) & 0xff);
+    _rfm69_writereg(RFM69_REGFRFLSB, frf & 0xff);
+}
+
+/* Get rfm69 mode */
+static uint8_t _rfm69_getmode(void)
+{
+    uint8_t RegOpMode;
+    RegOpMode = _rfm69_readreg(RFM69_REGOPMODE);
+    return (RegOpMode & 0b00011100) >> 2;
 }
 
 /* Change rfm69 opmode. Options are found under RegOpMode in rfm69.h */
-void rfm69_opmode(uint8_t opmode)
+static void _rfm69_setmode(uint8_t mode)
 {
-    if(opmode > RFM69_OPMODE_RX)
+    uint8_t RegOpMode;
+
+    if(mode > RFM69_OPMODE_RX)
         panic();
 
-    panic();
+    RegOpMode = _rfm69_readreg(RFM69_REGOPMODE);
+    RegOpMode &= 0b11100011;
+    RegOpMode |= mode << 2;
+    _rfm69_writereg(RFM69_REGOPMODE, RegOpMode);
+
+    /* Wait for new mode to take effect: */
+/*    volatile uint8_t themode=0xff;
+    while(themode != mode)
+    {
+        themode = _rfm69_getmode();
+        themode = themode & 0b01111111;
+    }*/
+    /*while(_rfm69_getmode() != mode);*/
 }
 
 /* Transmit bytes from buffer `buf', length `len' bytes.  This function is
  * synchronous - it will not return until transmission is complete */
 void rfm69_transmit(uint8_t *buf, uint8_t len)
 {
-    panic();
+    /* If we are in transmit, wait for transmit to finish */
+    while(_rfm69_getmode() == RFM69_OPMODE_TX);
+
+    /* If we are in sleep, fs or rx, bring us up to standby */
+    uint8_t mode = _rfm69_getmode();
+    if(mode == RFM69_OPMODE_SLEEP || mode == RFM69_OPMODE_FS ||
+       mode == RFM69_OPMODE_RX)
+    {
+        _rfm69_setmode(RFM69_OPMODE_STDBY);
+        while(_rfm69_getmode() != RFM69_OPMODE_STDBY);
+    }
+
+    /* Write the packet to the RFM69's FIFO */
+    _rfm69_bulkwrite(RFM69_REGFIFO, buf, len);
+
+    /* Initiate transmit */
+    _rfm69_setmode(RFM69_OPMODE_TX);
+
+    /* Block until transmit completes */
+    while(_rfm69_getmode() == RFM69_OPMODE_TX);
 }
 
 /* Receive `len' bytes into buffer `buf'.  This function
