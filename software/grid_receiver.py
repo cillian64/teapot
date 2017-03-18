ac3b9c8f5334bede6b9f2617e3e36a744863df6b
David Turner <cillian64@googlemail.com>
2017-03-18 14:43:49 +0000

    New grid receiver

--- software/grid_receiver.py
+++ software/grid_receiver.py
@@ -2,12 +2,14 @@
 
 import serial
 import sys
-import requests
 import numpy as np
+import grid_decode
+import time
+import matplotlib
+matplotlib.use("TkAgg")
 import matplotlib.pyplot as plt
-import matplotlib.cm as cm
 
-default_serial = "/dev/ttyACM2"
+default_serial = "/dev/cu.usbmodem401"
 
 if len(sys.argv) == 2:
     default_serial = sys.argv[1]
@@ -19,57 +21,38 @@ ser = serial.Serial(default_serial)
 allbytes = bytearray([0]*128)
 pixels = [0]*64
 array = np.zeros((8, 8))
-array[:] = 80
-array[0,0] = 120
+# Set initial scale from 20-30 celcius.
+array[:] = 20
+array[1] = 30
 
-fig = plt.figure()
-ax = fig.add_subplot(111)
 im = plt.imshow(array, cmap=plt.get_cmap("coolwarm"), interpolation="none")
 plt.colorbar()
-fig.show()
+plt.ion()
+plt.show()
 
 while True:
-    # Sync to input signal:
-    while True:
-        while ser.read().decode('utf-8') != 'G':
-            pass
-        if ser.read().decode('utf-8') != 'R':
-            continue
-        if ser.read().decode('utf-8') != 'I':
-            continue
-        if ser.read().decode('utf-8') != 'D':
-            continue
-        packet_type = ser.read().decode('utf-8')
-        if packet_type not in ['1', '2', '3']:
-            continue
-        break
+    line = ser.readline()[:-1].decode('utf-8')
+    if not line.endswith("[TEAx]"):
+        plt.pause(0.1)
+        continue
 
-    # Read correct number of bytes:
-    if packet_type == '1' or packet_type == '2':
-        packet = ser.read(43)
-    else:
-        packet = ser.read(42)
+    # Extract comment portion of packet:
+    line = line[line.find(":")+1:line.find("[")]
+    average, centre, pixels = grid_decode.decode(line)
 
-    strpacket = ' '.join([str(int(x)) for x in packet])
-#    print("Packet type {}: {}".format(packet_type, strpacket))
+    for y in range(8):
+        for x in range(8):
+            idx = 8 * y + x
+            array[7-y, 7-x] = pixels[idx]
+            print("{:2.2f} ".format(pixels[idx]), end="")
+            if (idx+1) % 8 == 0:
+                print("")
 
-    if packet_type == '1':
-        allbytes[0:43] = packet
-    elif packet_type == '2':
-        allbytes[43:86] = packet
-    elif packet_type == '3':
-        allbytes[86:128] = packet
+    print("Centre: {:.2f}°C".format(centre))
+    print("Average: {:.2f}°C".format(average))
+    print("")
 
-    for i in range(64):
-        pixels[i] = allbytes[2*i+1]# + allbytes[2*i-1] * 2**8
-
-    for x in range(8):
-        for y in range(8):
-            array[7-y,7-x] = pixels[8*y+x]
-
-#    print(' '.join([str(int(x)) for x in pixels]))
-#    print("")
-    print(array)
     im.set_data(array)
-#    plt.imshow(array, cmap=plt.get_cmap("coolwarm"))
+#    im.autoscale()
     plt.draw()
+    plt.pause(0.1)
